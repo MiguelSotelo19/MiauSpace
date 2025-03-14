eb24e9758ceae6032ba92c8b4db5593c9a0ce785
BryanDuranXO <20223tn134@utez.edu.com>
2025-03-14 02:26:27 -0600

    crud post

--- Back/MiauSpace/MiauSpace/urls.py
+++ Back/MiauSpace/MiauSpace/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mascotas/',include('usuario_mascota.urls') )
+    path('mascotas/',include('usuario_mascota.urls')),
+    path('posts/', include('post.urls'))
 ]
--- Back/MiauSpace/post/views.py
+++ Back/MiauSpace/post/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .models import Posts
+from rest_framework.renderers import JSONRenderer
+from .serializers import PostsSerializer
+class PostsViewset(viewsets.ModelViewSet):
+    queryset = Posts.objects.all()
+    serializer_class = PostsSerializer
+    renderer_classes = [JSONRenderer]
