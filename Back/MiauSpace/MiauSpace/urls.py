909d9188975b73f47864e31fd74e0006d8a4c3f3
BryanDuranXO <20223tn134@utez.edu.com>
2025-03-14 02:42:55 -0600

    crud comentarios

--- Back/MiauSpace/MiauSpace/urls.py
+++ Back/MiauSpace/MiauSpace/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mascotas/',include('usuario_mascota.urls')),
-    path('posts/', include('post.urls'))
+    path('posts/', include('post.urls')),
+    path('comentarios/', include('comentarios.urls'))
 ]
