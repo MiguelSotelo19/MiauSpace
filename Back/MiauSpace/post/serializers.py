e014830e493ec5e72d62755fff1930b597caafe8
BryanDuranXO <20223tn134@utez.edu.com>
2025-03-26 00:30:34 -0600

    cambios en el serializer de post

--- Back/MiauSpace/post/serializers.py
+++ Back/MiauSpace/post/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Posts
+from .models import Posts, Imagenes
 
 class PostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posts
+        fields = '__all__'
+        
+class PostImagenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Imagenes
         fields = '__all__'
--- Back/MiauSpace/post/urls.py
+++ Back/MiauSpace/post/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from .views import PostsViewset
+from .views import PostsViewset, PostsImgViewset
 
 router = SimpleRouter()
-router.register(r'api', PostsViewset)
+router.register(r'api', PostsViewset, basename='posts')  
+router.register(r'api-img', PostsImgViewset, basename='posts-img')  
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
 ]
