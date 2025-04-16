a7a502ec16e263af8930accb8345f43c6875ffda
BryanDuranXO <20223tn134@utez.edu.com>
2025-04-15 19:07:07 -0600

    modificacion de jwt y consulta personalizada

--- Back/MiauSpace/reacciones/views.py
+++ Back/MiauSpace/reacciones/views.py
@@ -3,8 +3,19 @@ from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
 from .models import Reaccion
 from .serializers import ReaccionSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ReaccionViewset(viewsets.ModelViewSet):
     queryset = Reaccion.objects.all()
     serializer_class = ReaccionSerializer
     renderer_classes = [JSONRenderer]
+    authentication_classes = [JWTAuthentication]
+    permission_classes=[IsAuthenticated]
+    
+    def get_permissions(self):
+        if self.request.method in ['GET','POST','PUT', 'DELETE', 'PATCH']:
+            # Checar si tenemos sesión 
+            return [IsAuthenticated()]
+        #Dar acceso a todo lo demas sin estar logueado
+        return []
